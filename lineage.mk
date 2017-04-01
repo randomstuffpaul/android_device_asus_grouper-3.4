23502463fffc8d43ec09e3f72678a2b9456031ef
Harry Youd <harry@harryyoud.co.uk>
2017-04-01 18:20:39 +0100

    Stuff
    Change-Id: I37da86de5d4192adcb17e18a18f16058e786ee64

--- lineage.mk
+++ lineage.mk
@@ -9,7 +9,7 @@ TARGET_SCREEN_WIDTH := 800
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/asus/grouper/full_grouper.mk)
+$(call inherit-product, device/asus/grouper/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grouper
@@ -20,4 +20,3 @@ PRODUCT_MANUFACTURER := asus
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:5.1/LMY47D/1743759:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 5.1 LMY47D 1743759 release-keys"
-
